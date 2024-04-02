e70f11a30a3d99f8e9d380e69537fc7393fd0e6a
razzzz1 <ryanmaiyo1@gmail.com>
2024-04-02 12:25:19 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+const  street = 42;
+const conv = 264;
+function distanceFromHqInBlocks (blocks) {
+    return Math.abs(blocks-street);
+    }
+  
+  
+  function distanceFromHqInFeet (blocks) {
+     distanceFromHqInBlocks(blocks);
+    let disinfeet = distanceFromHqInBlocks(blocks) * conv;
+    return disinfeet;
+  }
+  
+  function distanceTravelledInFeet (start, destination) {
+    return Math.abs(destination - start) * conv;
+  }
+  function distanceTravelledInFeet (start, destination) {
+    return Math.abs(destination - start) * conv;
+  }
+  function calculatesFarePrice (start, destination) {
+    let distance = Math.abs(destination - start) * conv;
+    if (distance > 2500) {
+        return 'cannot travel that far';
+    } else if (distance > 2000) {
+        return 25;
+    } else if (distance > 400) {
+        let chargabledistance = distance - 400;
+        return chargabledistance * 0.02;
+    } else{
+        return 0;
+    }
+}
+
